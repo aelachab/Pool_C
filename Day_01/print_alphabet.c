9ddf298ee1f16915e145008b5a4b4cbfa094acaf
Ayoub El achab <aelachab21@gmail.com>
2025-11-24 16:11:56 +0100

    Day_1 Done

---
+++ Day_01/print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void print_alphabet(){
+
+    for (char i = 'a'; i <= 'z'; i++)
+    {
+       write(1,&i,1);
+    }
+
+}
+
+int main (){
+
+  print_alphabet();
+  return 0;
+}
