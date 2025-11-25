7033bf0f81e7f401401f6d6a2697b84ac1c92e09
Ayoub El achab <aelachab21@gmail.com>
2025-11-25 14:49:36 +0100

    Day 1 Completed

--- Day_01/C01/character.c
+++ Day_01/C01/character.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
-
  char print_character(char x) {
     write(1,&x,1);
 
 }
  
 int main () {
-    char character = 's';
+    char character ='s';
     print_character(character);
 }
--- Day_01/C02/print_alphabet.c
+++ Day_01/C02/print_alphabet.c
@@ -2,14 +2,12 @@
 #include <unistd.h>
 
 void print_alphabet(){
-
     for (char i = 'a'; i <= 'z'; i++)
     {
        write(1,&i,1);
     }
 
 }
-
 int main (){
 
   print_alphabet();
